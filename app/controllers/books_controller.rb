f8467d431e6d7cad72d2fdfa541dcba90b6052eb
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-06-01 15:46:27 -0400

    books edit form and patch routes established

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,11 +21,20 @@ class BooksController < ApplicationController
     end
 
     get '/books/:id/edit' do
-        
+        @book = Book.find_by_id(params[:id])
+        erb :'books/edit'
     end
 
+    patch '/books/:id' do
+        @book = Book.find_by_id(params[:id])
+        @book.title = params[:title]
+        @book.author = params[:author]
+        @book.genre = params[:genre]
+        @book.year_published = params[:year_published]
+        @book.save
 
-
+        redirect "/books/#{@book.id}"
+    end
 
 
 end
