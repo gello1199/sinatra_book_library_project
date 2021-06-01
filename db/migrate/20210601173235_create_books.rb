c62f80d72012455d6c1f123c6dfe361eb5ebd99f
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-06-01 13:35:31 -0400

    user and books migration

---
+++ db/migrate/20210601173235_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :genre
+      t.string :year_published
+      t.integer :user_id
+    end
+  end
+end
