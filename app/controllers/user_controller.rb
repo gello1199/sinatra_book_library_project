996ea5704e9cfb38fa401772c4f2e89bafa037c7
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-06-01 18:46:30 -0400

    login route and form added

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,7 +5,25 @@ class UserController < ApplicationController
         erb :'users/index'
     end
 
-    
+    get '/users/signup' do
+        erb :'users/signup'
+    end
+
+    post '/users/signup' do
+        user = User.new(params)
+
+        redirect '/signup'
+    end
+
+    get '/users/login' do
+        erb :'users/login'
+    end
+
+    post '/users/login' do
+        user = User.new(params)
+
+        redirect '/login'
+    end
 
 
 end
